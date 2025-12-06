b04419151016697bcbec327f029a3f10ea90012a
userwanyong <2026804718@qq.com>
2025-12-06 16:13:31 +0800

    feat: 瑞吉外卖用户端/管理端所有接口

--- src/main/java/cn/wanyj/rjwm/controller/UserController.java
+++ src/main/java/cn/wanyj/rjwm/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
 
@@ -90,6 +91,12 @@ public class UserController {
             return R.success(user);
         }
         return R.error("登录失败");
+    }
+
+    @PostMapping("/loginout")
+    public R<String> logout(HttpServletRequest request) {
+        request.getSession().removeAttribute("user");
+        return R.success("退出成功");
     }
 
 }
